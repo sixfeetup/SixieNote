c36857fe76c57cc2292ffd632bcdc2d8c2d8d858
Scot Kronenfeld <scotwk@gmail.com>
2015-05-04 18:44:19 -0400

    Improving index view to do real stuff

--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Note
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the notes index.")
+    latest_note_list = Note.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('note/index.html')
+    context = RequestContext(request, {
+        'latest_note_list': latest_note_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, note_id):
     return HttpResponse("You're looking at note %s." % note_id)
