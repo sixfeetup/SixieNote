b50ec9fa265cfd803fea2855aa79a5b8147b5eb7
Scot Kronenfeld <scotwk@gmail.com>
2015-05-04 18:45:49 -0400

    Using render shortcut in views.py

--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -1,17 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 from .models import Note
 
 
 def index(request):
     latest_note_list = Note.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('note/index.html')
-    context = RequestContext(request, {
+    context = {
         'latest_note_list': latest_note_list,
-    })
-    return HttpResponse(template.render(context))
+    }
+    return render(request, 'note/index.html', context)
 
 def detail(request, note_id):
     return HttpResponse("You're looking at note %s." % note_id)
