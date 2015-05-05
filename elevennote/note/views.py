4c6883b99fd966f25e0734dcbe70e7afa150ce93
Scot Kronenfeld <scotwk@gmail.com>
2015-05-05 16:14:37 -0400

    Removed unneeded imports

--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -1,5 +1,3 @@
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 
 from .models import Note
