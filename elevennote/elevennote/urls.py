cd5b9f51987aac1097c5239de90cb3b362bf0a2a
Scot Kronenfeld <scotwk@gmail.com>
2015-05-04 23:00:45 -0400

    Added in user authentication

--- elevennote/elevennote/urls.py
+++ elevennote/elevennote/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout),
+
     url(r'^notes/', include('note.urls', namespace="note")),
 ]
--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .models import Note
 
 
+@login_required
 def index(request):
+    print(request.user.is_authenticated())
+    print(request.user)
     latest_note_list = Note.objects.order_by('-pub_date')[:5]
     context = {
         'latest_note_list': latest_note_list,
     }
     return render(request, 'note/index.html', context)
 
+
+@login_required
 def detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'note/detail.html', {'note': note})
