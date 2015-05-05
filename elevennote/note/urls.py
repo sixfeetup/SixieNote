10cd1bc399c789ce59598e4db86d2ce14d89a931
Scot Kronenfeld <scotwk@gmail.com>
2015-05-05 14:25:21 -0400

    Adding a page to create new notes

--- elevennote/note/urls.py
+++ elevennote/note/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import NoteList, NoteDetail
+from .views import NoteList, NoteDetail, NoteCreate
 
 urlpatterns = [
     url(r'^$', NoteList.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', NoteDetail.as_view(), name='detail'),
+    url(r'^new/$', NoteCreate.as_view(), name='create'),
 ]
--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -1,10 +1,12 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.utils import timezone
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Note
 from .mixins import LoginRequiredMixin
-
+from .forms import NoteForm
 
 class NoteList(LoginRequiredMixin, ListView):
     paginate_by = 5
@@ -20,3 +22,14 @@ class NoteDetail(LoginRequiredMixin, DetailView):
     template_name = 'note/detail.html'
     context_object_name = 'note'
 
+
+class NoteCreate(LoginRequiredMixin, CreateView):
+    form_class = NoteForm
+    template_name = 'note/form.html'
+    success_url = reverse_lazy('note:index')
+
+    def form_valid(self, form):
+        print(form)
+        form.instance.owner = self.request.user
+        form.instance.pub_date = timezone.now()
+        return super(NoteCreate, self).form_valid(form)
