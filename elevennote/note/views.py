96846057126cf8d7933ad8c161fba0fb376b4ace
Scot Kronenfeld <scotwk@gmail.com>
2015-05-05 10:38:40 -0400

    Move login_required to a mixin

--- elevennote/note/views.py
+++ elevennote/note/views.py
@@ -3,26 +3,20 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 
 from .models import Note
+from .mixins import LoginRequiredMixin
 
 
-class NoteList(ListView):
+class NoteList(LoginRequiredMixin, ListView):
     paginate_by = 5
     template_name = 'note/index.html'
     context_object_name = 'latest_note_list'
-
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(NoteList, self).dispatch(*args, **kwargs)
 
     def get_queryset(self):
         return Note.objects.filter(owner=self.request.user)
 
 
-class NoteDetail(DetailView):
+class NoteDetail(LoginRequiredMixin, DetailView):
     model = Note
     template_name = 'note/detail.html'
     context_object_name = 'note'
 
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(NoteDetail, self).dispatch(*args, **kwargs)
