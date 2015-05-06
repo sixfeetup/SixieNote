4b64e21e9969f22e6e308486d14363eaa961a4d2
Scot Kronenfeld <scotwk@gmail.com>
2015-05-06 11:05:42 -0400

    Adding in a restriction to not allow users to see anything except their own notes.

--- elevennote/note/api/resources.py
+++ elevennote/note/api/resources.py
@@ -2,8 +2,10 @@ from django.contrib.auth.models import User
 
 from tastypie import fields
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import Authorization
 
+from .authorization import UserObjectsOnlyAuthorization
 from ..models import Note
 
 
@@ -12,6 +14,8 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         allowed_methods = ['get']
         resource_name = 'user'
+        authentication = BasicAuthentication()
+        authorization = Authorization()
         excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
         filtering = {
             'username': ALL,
@@ -24,6 +28,8 @@ class NoteResource(ModelResource):
     class Meta:
         queryset = Note.objects.all()
         allowed_methods = ['get']
+        authentication = BasicAuthentication()
+        authorization = UserObjectsOnlyAuthorization()
         filtering = {
             'owner': ALL_WITH_RELATIONS,
             'pub_date': ['exact', 'lt', 'lte', 'gte', 'gt'],
