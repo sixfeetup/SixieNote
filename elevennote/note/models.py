fd24c9c90270816434b7a49a167f89ce43d15bd4
Scot Kronenfeld <scotwk@gmail.com>
2015-05-13 13:59:44 -0400

    Adding a first test

--- elevennote/note/models.py
+++ elevennote/note/models.py
@@ -14,4 +14,5 @@ class Note(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timedelta(days=1)
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
