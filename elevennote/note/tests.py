06667de63c7d893b0ce75bde518723de39519d0c
Scot Kronenfeld <scotwk@gmail.com>
2015-05-13 13:59:44 -0400

    Adding a first test

--- elevennote/note/tests.py
+++ elevennote/note/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
+from .models import Note
+
 # Create your tests here.
+class NoteMethodTests(TestCase):
+
+    def test_was_published_recently(self):
+        """
+        was_published_recently() should return False for notes whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_note = Note(pub_date=time)
+        self.assertEqual(future_note.was_published_recently(), False)
