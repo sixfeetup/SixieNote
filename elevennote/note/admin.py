3658bd461817d193aa0aa3c2653f496116ed94a8
Scot Kronenfeld <scotwk@gmail.com>
2015-05-12 22:11:45 -0400

    Removing uneeded things on 00 branch

--- elevennote/note/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'owner', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-
-# Register your models here.
-admin.site.register(Note, NoteAdmin)
--- elevennote/note/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Note
-
-class NoteForm(forms.ModelForm):
-
-    class Meta:
-        model = Note
-        exclude = ['owner', 'pub_date']
