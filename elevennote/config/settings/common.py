c6bd142595e20afcab86424e023c80932f459ed4
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2017-01-01 16:21:39 -0500

    better defaults for the ckeditor

--- elevennote/config/settings/common.py
+++ elevennote/config/settings/common.py
@@ -233,3 +233,14 @@ ADMIN_URL = r'^admin/'
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = 'pillow'
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ]
+    }
+}
