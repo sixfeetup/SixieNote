dd85019dec7f6c2fb43e5ca30aa726f578e94100
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2017-01-07 16:19:08 -0500

    We want to redirect to the default notes app if there isn't a `next` specified

--- elevennote/config/settings/common.py
+++ elevennote/config/settings/common.py
@@ -230,6 +230,9 @@ ADMIN_URL = r'^admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+
+LOGIN_REDIRECT_URL = '/notes/'
+
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = 'pillow'
