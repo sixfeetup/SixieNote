57f2f46ba97461505aa6dde2a0b5f89cd2d0f7e7
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2020-04-08 16:52:41 -0400

    Code formatting cleaning

--- elevennote/elevennote/note/auth_views.py
+++ elevennote/elevennote/note/auth_views.py
@@ -2,18 +2,19 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
 
-class RegisterView(FormView):
-   template_name = 'registration/register.html'
-   form_class = UserCreationForm
-   success_url='/'
 
-   def form_valid(self, form):
-      #save the new user first
-      form.save()
-      #get the username and password
-      username = self.request.POST['username']
-      password = self.request.POST['password1']
-      #authenticate user then login
-      user = authenticate(username=username, password=password)
-      login(self.request, user)
-      return super(RegisterView, self).form_valid(form)
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        # save the new user first
+        form.save()
+        # get the username and password
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        # authenticate user then login
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
